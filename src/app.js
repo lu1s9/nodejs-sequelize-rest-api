227775cca30aa126569f41c830729258f24597d2
Luis Fernando <luis@lu1s9.xyz>
2023-04-06 20:38:56 -0600

    404 responses

--- src/app.js
+++ src/app.js
@@ -8,4 +8,8 @@ app.use(express.json());
 app.use('/projects/', projectRoutes);
 app.use('/tasks/', taskRoutes);
 
+app.use((req, res) => {
+  return res.status(404).json({ message: "Sorry, can't find that!" });
+});
+
 export default app;
