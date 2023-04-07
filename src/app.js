914e7764e3b5f153b7b14bb9a827be7f7f9c4714
Luis Fernando <luis@lu1s9.xyz>
2023-04-06 20:43:55 -0600

    helmet security

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import helmet from 'helmet';
 import projectRoutes from './routes/projects.routes.js';
 import taskRoutes from './routes/task.routes.js';
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use('/projects/', projectRoutes);
 app.use('/tasks/', taskRoutes);
